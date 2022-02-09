519c4c93594aea4765b89b6b30f90b5903fcc539
lixavi <121849896+lixavi@users.noreply.github.com>
2022-02-09 12:30:21 +0530

    updated

--- test.py
+++ test.py
@@ -8,3 +8,9 @@ def test_gan(generator, num_samples, latent_dim, device, output_dir):
     with torch.no_grad():
         latent_vectors = torch.randn(num_samples, latent_dim).to(device)
         generated_images = generator(latent_vectors).cpu()
+
+    # Save generated images
+    for i in range(num_samples):
+        save_image(generated_images[i], f"{output_dir}/generated_image_{i+1}.png")
+
+    print(f"Generated {num_samples} images saved in {output_dir}")
